f2db705f302772f9c7a18d089f7fa5d696b77bfa
YuldashovMM <ymansurbek2212@gmail.com>
2022-01-20 14:56:44 +0300

    JavaScript

---
+++ ROBOCODE (FrontEnd JavaScript)/1-dars 19012022/js/script.js
@@ -0,0 +1,14 @@
+// alert("Say Hello")
+// var,let, const
+// ECMASCRIPT 5-2009
+// var x = 10;
+// var x = 20;
+// alert(x)
+// ECMASCRIPT 6-2015
+// let y;
+// const x;
+// alert()
+// confirm()
+// let name = prompt('Ism')
+// alert(name)
+console.log(20 + 10)
